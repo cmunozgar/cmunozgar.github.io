ff96c4a7fbaea1a491d4565eeaf708945bf72d77
Carlos Muñoz <cmunozgar@gmail.com>
2020-06-28 11:35:06 +0200

    css and copy fixes

--- gulpfile.js
+++ gulpfile.js
@@ -6,88 +6,100 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var cp = require('child_process');
 var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync');
+var browser_sync = require('browser-sync');
+const del = require('del');
 
-var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'bundle';
+// BrowserSync
+function browserSync(done) {
+  browser_sync.init({
+    server: {
+      baseDir: "./_site/"
+    },
+    port: 4000
+  });
+  done();
+}
 
-/*
- * Build the Jekyll Site
- * runs a child process in node that runs the jekyll commands
- */
-gulp.task('jekyll-build', function (done) {
-	return cp.spawn(jekyllCommand, ['exec', 'jekyll', 'build'], {stdio: 'inherit'}).on('close', done);
-});
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browser_sync.reload();
+  done();
+}
 
-/*
- * Rebuild Jekyll & reload browserSync
- */
-gulp.task('jekyll-rebuild', gulp.series('jekyll-build', function () {
-	browserSync.reload();
-}));
+// Clean assets
+function clean() {
+  return del(["./_site/assets/"]);
+}
 
-/*
- * Build the jekyll site and launch browser-sync
- */
-gulp.task('browser-sync', gulp.series('jekyll-build', function() {
-	browserSync({
-		server: {
-			baseDir: '_site'
-		}
-	});
-}));
-
-/*
-* Compile and minify sass
-*/
-gulp.task('sass', function(done) {
-  	gulp.src('src/styles/**/*.scss')
+// CSS task
+function css() {
+  return gulp
+		.src('src/styles/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(csso())
 		.pipe(gulp.dest('assets/css/'));
-		done();
-});
+}
 
-/*
-* Compile fonts
-*/
-gulp.task('fonts', function(done) {
-	gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
+// Fonts task
+function fonts(){
+	return gulp
+		.src('src/fonts/**/*.{ttf,woff,woff2}')
 		.pipe(plumber())
 		.pipe(gulp.dest('assets/fonts/'));
-		done();
-})
+}
 
-/*
- * Minify images
- */
-gulp.task('imagemin', function(done) {
-	return gulp.src('src/img/**/*.{jpg,png,gif}')
+// Optimize Images
+function images() {
+	return gulp
+		.src('src/img/**/*.{jpg,png,gif}')
 		.pipe(plumber())
 		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
 		.pipe(gulp.dest('assets/img/'));
-		done();
-});
+}
 
-/**
- * Compile and minify js
- */
-gulp.task('js', function(done){
+// Scripts task
+function scripts(){
 	return gulp.src('src/js/**/*.js')
 		.pipe(plumber())
 		.pipe(concat('main.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js/'));
-		done();
-});
+		.pipe(gulp.dest('assets/js/'));;
+}
 
-gulp.task('watch', function(done) {
-	gulp.watch('src/styles/**/*.scss', ['sass', 'jekyll-rebuild']);
-	gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/fonts/**/*.{tff,woff,woff2}', ['fonts']);
-	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-	gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
-	done();
-});
+function watchFiles() {
+	gulp.watch('src/styles/**/*.scss', gulp.series(css, jekyll, browserSyncReload));
+	gulp.watch('src/js/**/*.js', scripts);
+	gulp.watch('src/fonts/**/*.{tff,woff,woff2}', fonts);
+	gulp.watch('src/img/**/*.{jpg,png,gif}', images);
+	gulp.watch(
+		[
+      "./_includes/**/*",
+      "./_layouts/**/*",
+      "./_pages/**/*",
+      "./_posts/**/*",
+      "./_projects/**/*"
+    ],
+    gulp.series(jekyll, browserSyncReload)
+	);
+}
 
-gulp.task('default', gulp.series('js', 'sass', 'fonts', 'browser-sync', 'watch'));
+// Jekyll
+function jekyll() {
+  return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
+}
+
+// define complex tasks
+const js = gulp.series(scripts);
+const build = gulp.series(clean, gulp.parallel(css, images, jekyll, js));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.images = images;
+exports.css = css;
+exports.js = js;
+exports.jekyll = jekyll;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
